6123c582e86907d8ccf50c306ea6729e11cf8d4e
davbraun <davbraun@bu.edu>
2017-03-20 16:23:35 -0400

    Tried to do API

--- Application/routes/index.js
+++ Application/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title : "Search"});
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+    var searchQuery = req.body.searchbox;
+
+    // make API call
+
 });
 
 module.exports = router;
